a531df3d61453a293b5998095b8eed2d6217e1c7
Scott Wolfe <swolfe8@gatech.edu>
2017-02-19 10:05:18 -0500

    added getView, setView, getModel, setModel methods

--- src/com/github/scottswolfe/kathyscleaning/interfaces/Controller.java
+++ src/com/github/scottswolfe/kathyscleaning/interfaces/Controller.java
@@ -2,4 +2,11 @@ package com.github.scottswolfe.kathyscleaning.interfaces;
 
 public interface Controller {
 
+    public void setView(Object obj);
+    
+    public Object getView();
+    
+    public void setModel(Object obj);
+    
+    public Object getModel();
 }
