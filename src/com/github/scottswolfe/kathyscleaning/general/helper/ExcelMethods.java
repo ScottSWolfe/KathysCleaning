db8517a9ac43d493c9b6e4e33add07348a462cf6
Scott Wolfe <swolfe8@gatech.edu>
2022-08-18 12:24:04 -0400

    Exit program after generating and opening excel document

--- src/com/github/scottswolfe/kathyscleaning/general/helper/ExcelMethods.java
+++ src/com/github/scottswolfe/kathyscleaning/general/helper/ExcelMethods.java
@@ -23,10 +23,10 @@ public class ExcelMethods {
     /**
      * Changes the given excel String to explicitly contain the worked hours
      * instead of having a reference to the hours worked cell
-     * 
+     *
      * Formula input format:  =+VLOOKUP(O$2,PAY!$B$3:$C$16,2,FALSE)*$C12
      * Formula return format: =+VLOOKUP(O$2,PAY!$B$3:$C$16,2,FALSE)*1.362
-     * 
+     *
      * @param s the formula
      * @param hours the number of hours worked
      * @return the adjusted formula as a String
@@ -36,12 +36,12 @@ public class ExcelMethods {
         String[] formula_pieces = formula.split("[*]");
         return formula_pieces[0] + "*" + string_to_add;
     }
-    
+
     private static String doubleToStringWith3DigitsAfterDecimal(Double d) {
         DecimalFormat numberFormat = new DecimalFormat("#.000");
         return numberFormat.format(d);
     }
-    
+
     @SuppressWarnings({ "rawtypes", "unchecked" })
     public static void chooseFileAndGenerateExcelDoc(Controller controller) {
 
@@ -52,12 +52,12 @@ public class ExcelMethods {
         }
 
         JOptionPane.showMessageDialog(null, "Now save the new Excel document.");
-        
+
         File file = FileChooserHelper.saveAs(
                     SettingsModel.getExcelSaveLocation(),
                     getDefaultSaveFileName(),
                     FileChooserHelper.XLSX);
-        
+
         if (file != null) {
             GeneralExcelHelper.generateExcelDocument(file);
             try {
@@ -66,13 +66,13 @@ public class ExcelMethods {
             } catch (IOException e1) {
                 JOptionPane.showMessageDialog(new JFrame(), "The Excel document could not be opened automatically.");
             }
-            return;
+            System.exit(0);
         }
-        
+
         controller.initializeForm(
                 new GeneralController(controller.getFormType()));
     }
-    
+
     private static String getDefaultSaveFileName() {
         if (SessionModel.isSaveFileChosen()) {
             String fileName = SessionModel.getSaveFile().getName();
@@ -83,5 +83,5 @@ public class ExcelMethods {
                     FileChooserHelper.XLSX);
         }
     }
-    
+
 }
