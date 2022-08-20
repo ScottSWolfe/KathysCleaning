af6a91b298633145b7fa7942ba760d89e9935b28
Scott Wolfe <swolfe8@gatech.edu>
2022-08-20 10:07:33 -0400

    Get scheduled sheet by name instead of index

--- src/com/github/scottswolfe/kathyscleaning/scheduled/controller/ScheduledExcelHelper.java
+++ src/com/github/scottswolfe/kathyscleaning/scheduled/controller/ScheduledExcelHelper.java
@@ -19,55 +19,57 @@ import com.github.scottswolfe.kathyscleaning.scheduled.model.WorkerSchedule;
 
 public class ScheduledExcelHelper implements ExcelHelper<NW_Data> {
 
+    private static final String SCHEDULED_SHEET_NAME = "PRINTOUTS";
+
     @Override
     public void writeModelToExcel(NW_Data model, XSSFWorkbook wb) {
 
-        Sheet sheet = wb.getSheetAt(2);
-        
+        final Sheet sheet = wb.getSheet(SCHEDULED_SHEET_NAME);
+
         // 2) input schedules for each employee on each day
         int n_column = 3;       // column with worker names is column 3
         int d_column = 1;       // column with days is column 1
-        
+
         Row nrow;               // current row in looking for worker names
         Row drow;               // current row in looking for days
-        
+
         int nnum;
         int dnum;
-        
+
         String s1;
         String s2;
-        
+
         WorkerSchedule ws;
         String[] weekday = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
-        
+
         NW_DayData[] days = model.dayData;
-        
+
         // for each day
         for(int d = 0; d < 5; d++){
-            
+
             NW_DayData dayData = days[d];
-            
+
             // for each employee
             List<WorkerSchedule> workSchedules = dayData.getWorkerSchedule();
             for (int w = 0; w < workSchedules.size(); w++) {
-                
+
                 ws = workSchedules.get(w);
-                
+
                 nnum = 0;
                 nrow = sheet.getRow(nnum);
 
                 boolean d1 = false;
                 boolean d2 = false;
-                                
+
                 // while worker name has not been found
                 while (!d1) {
-                    
+
                     if (nrow != null) {
-                        
+
                         if (nrow.getCell(n_column) != null &&
                             nrow.getCell(n_column).getCellType() == Cell.CELL_TYPE_FORMULA &&
                             nrow.getCell(n_column).getCachedFormulaResultType() == Cell.CELL_TYPE_STRING)
-                        {    
+                        {
                             s1 = nrow.getCell(n_column).getRichStringCellValue().getString();//getStringCellValue(); // NOTE: this gets the last cached value of the formula
                         }
                         else {
@@ -77,16 +79,16 @@ public class ScheduledExcelHelper implements ExcelHelper<NW_Data> {
                     else {
                         s1 = null;
                     }
-                    
+
                     // compare strings to worker name; if name is found
                     if (ws.isEquivalent(s1)) {
-                        
+
                         dnum = nnum + 1;                    //begin row after name was found
                         drow = sheet.getRow(dnum);
 
                         // while correct day of week has not been found
                         while ( !d2 ) {
-                            
+
                             if (drow != null) {
                                 if (drow.getCell(d_column) != null && drow.getCell(d_column).getCellType() == Cell.CELL_TYPE_STRING) {
                                     s2 = drow.getCell(d_column).getStringCellValue();
@@ -98,16 +100,16 @@ public class ScheduledExcelHelper implements ExcelHelper<NW_Data> {
                             else {
                                 s2 = "";
                             }
-                                                
+
                             // compare strings in succeeding rows and column 1? to String[] s = [Monday, Tuesday, Wednesday, Thursday, Friday]
                             // if day is found
                             if ( s2.equals( weekday[d] ) ) {
-                            
+
                                 // move over one cell and input schedule
                                 drow.getCell(d_column + 1).setCellValue( ws.getSchedule() );
                                 d1 = true;
                                 d2 = true;
-                                
+
                             }
                             dnum++;
                             drow = sheet.getRow( dnum );
@@ -116,7 +118,7 @@ public class ScheduledExcelHelper implements ExcelHelper<NW_Data> {
                                 d2 = true;
                             }
                         }
-                        
+
                     }
                     nnum++;
                     nrow = sheet.getRow(nnum);
@@ -125,30 +127,30 @@ public class ScheduledExcelHelper implements ExcelHelper<NW_Data> {
                         d1 = true;
                     }
                 }
-                
+
             }
-        }        
-        makeUnusedCellsBlank(wb, sheet);        
+        }
+        makeUnusedCellsBlank(wb, sheet);
     }
-    
-    private void makeUnusedCellsBlank(XSSFWorkbook wb, Sheet sheet) {
+
+    private void makeUnusedCellsBlank(XSSFWorkbook wb, final Sheet sheet) {
 
         FormulaEvaluator evaluator = wb.getCreationHelper().createFormulaEvaluator();
         Cell cell;
         CellValue value;
-        
+
         int house_column = 3;
         int hours_column = 4;
         int total_column = 5;
         int row = 0;
-        
+
         int counter = 0;
         while (counter < 50) {
             cell = getCellSafely(sheet, row, hours_column, evaluator);
             if (cell != null) {
                 value = evaluator.evaluate(cell);
                 if (value.getCellType() == Cell.CELL_TYPE_NUMERIC) {
-                    if (value.getNumberValue() == 0) {                        
+                    if (value.getNumberValue() == 0) {
                         safeMakeCellBlank(sheet, row, house_column);
                         safeMakeCellBlank(sheet, row, hours_column);
                         safeMakeCellBlank(sheet, row, total_column);
@@ -160,16 +162,16 @@ public class ScheduledExcelHelper implements ExcelHelper<NW_Data> {
             counter++;
         }
     }
-        
-    private void safeMakeCellBlank(Sheet sheet, int row, int col) {
+
+    private void safeMakeCellBlank(final Sheet sheet, int row, int col) {
         try {
             sheet.getRow(row).getCell(col).setCellValue("");
         } catch (NullPointerException e) {
             e.printStackTrace();
         }
     }
-    
-    private Cell getCellSafely(Sheet sheet, int r, int c, FormulaEvaluator evaluator) {
+
+    private Cell getCellSafely(final Sheet sheet, int r, int c, FormulaEvaluator evaluator) {
         if (sheet == null) {
             return null;
         }
@@ -187,5 +189,5 @@ public class ScheduledExcelHelper implements ExcelHelper<NW_Data> {
         }
         return cell;
     }
-        
+
 }
